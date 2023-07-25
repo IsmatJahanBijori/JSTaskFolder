d012035f1e327ca1c54f56ebe3adfad6ffa92426
IsmatJahanBijori <ismatjahanbijori@gmail.com>
2023-07-25 17:40:43 +0600

    added

---
+++ task5.js
@@ -0,0 +1,46 @@
+//Task 5: Implement a simple JavaScript calculator. The calculator should take two numbers and an operator (+, -, *, /) as input and return the result of the operation.
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function calculator(num1, operator, num2) {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    case '/':
+      if (num2 !== 0) {
+        return num1 / num2;
+      } else {
+        throw new Error("Division by zero is not allowed!");
+      }
+    default:
+      throw new Error("Invalid operator!");
+  }
+}
+
+rl.question("Enter the first number: ", (num1) => {
+  rl.question("Enter the operator (+, -, *, /): ", (operator) => {
+    rl.question("Enter the second number: ", (num2) => {
+      try {
+        num1 = parseFloat(num1);
+        num2 = parseFloat(num2);
+
+        const result = calculator(num1, operator, num2);
+        console.log("Result:", result);
+        rl.close();
+      } catch (error) {
+        console.error("Error:", error.message);
+        rl.close();
+      }
+    });
+  });
+});
+
+  
